43464cdc88b9b71af86caf15b77e07be30bc628a
Andre Couto <andrecouto.jc.95@gmail.com>
2021-04-11 21:49:57 -0300

    html e php prontos - faz e valida as 4 operações com 2 números

---
+++ Calculadora/calculadora.php
@@ -0,0 +1,38 @@
+<?php
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $resultado = 0;
+
+    if ($_SERVER["REQUEST_METHOD"]  != "POST") {
+        echo "Método inválido";
+    }else if(!ctype_digit($num1)) {
+        echo "1º número -> inválido<br>";
+        echo "ERRO! Preencha novamente o formulário!";
+    }else if(!ctype_digit($num2)){
+        echo "2º número -> inválido<br>";
+        echo "ERRO! Preencha novamente o formulário!";        
+    }else {
+        $operacao = $_POST['operacao'];
+        switch($operacao){
+            case "soma" :
+                $resultado = $num1 + $num2;
+                echo $num1." + ".$num2. " = ".$resultado;
+                break;
+    
+            case "sub" :
+                $resultado = $num1 - $num2;
+                echo $num1." - ".$num2. " = ".$resultado;
+                break;
+    
+            case "multi" :
+                $resultado = $num1 * $num2;
+                echo $num1." * ".$num2. " = ".$resultado;
+                break;
+    
+            case "div" :
+                $resultado = $num1 / $num2;
+                echo $num1." / ".$num2. " = ".$resultado;
+                break;
+        }
+    }
+?>
